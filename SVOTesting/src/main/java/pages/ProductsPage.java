bf700171e4a611a22cdee7fc8aec3dcccc4f3e73
Robert Moritz <robbiemoritz@yahoo.com>
2020-02-24 10:26:26 -0600

    Removed duplicate files and added to gitignore

--- SVOTesting/src/main/java/pages/ProductsPage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class ProductsPage {
-    private WebDriver driver;
-
-    public ProductsPage(WebDriver driver){this.driver = driver;}
-}
