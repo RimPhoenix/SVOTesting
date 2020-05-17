1ebd2275d77e81445bfe37e187da98b1cd41c12d
Robert Moritz <robbiemoritz@yahoo.com>
2020-05-17 16:53:53 -0500

    Created InventoryPage, and InventoryTests Added Tests to add products then remove them

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -1,9 +1,63 @@
 package pages;
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InventoryPage {
 
-    public InventoryPage(WebDriver driver){
+    private WebDriver driver;
+
+    public InventoryPage(WebDriver driver)  {this.driver = driver;}
+    public String getTitle() {
+        WebDriverWait wait = new WebDriverWait(driver, 6);
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(addProductsBtn));
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return driver.getTitle();
     }
+
+
+
+        private By addProductsBtn = By.xpath("//button[@class='btn btn-primary js-confirm-this']");
+        private By pleaseSelectList = By.xpath("//span[contains(text(),'Please select')]");
+        private By emptyProductField = By.xpath("//html//body//span//span//span//input");
+        private By submitProductBtn = By.xpath("//div[@class='modal-footer']//input[@name='commit']");
+        private By searchField = By.xpath("//label[contains(text(),'Search:')]//input");
+        private By trashNewProduct = By.xpath("//a[contains(@class,'btn text-primary')]//*[local-name()='svg']");
+
+
+
+
+    public void setAddProductsBtn(){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(addProductsBtn)).click();
+   }
+
+    public void setPleaseSelectList(){WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(pleaseSelectList)).click();}
+
+    public void setEmptyProductField(){WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(emptyProductField)).click();}
+
+    public void setEmptyProductFieldWithProduct(String product){driver.findElement(emptyProductField).sendKeys(product);}
+
+    public void setSubmitProductBtn(){driver.findElement(submitProductBtn).click();}
+
+    public void setSearchField(String product){driver.findElement(searchField).sendKeys(product);}
+
+    public void setTrashNewProduct(){WebDriverWait wait = new WebDriverWait(driver, 3);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        wait.until(ExpectedConditions.presenceOfElementLocated(trashNewProduct));
+        driver.findElement(trashNewProduct).click();}
+
+    public String alert_getText(){return driver.switchTo().alert().getText();}
+
+    public void alert_clickToAccept(){driver.switchTo().alert().accept();}
+
+
 }
--- src/test/java/inventory/InventoryTests.java
+++ src/test/java/inventory/InventoryTests.java
@@ -1,8 +1,18 @@
 package inventory;
 
 import base.BaseTests;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import pages.InventoryPage;
 import pages.LoginPage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 
 public class InventoryTests extends BaseTests {
 
@@ -12,6 +22,31 @@ public class InventoryTests extends BaseTests {
     public void clickInventory(){
         LoginPage.adminLogin();
         var inventory = homePage.clickInventory();
+
     }
+
+    @Test
+    public void inventoryCycle() throws InterruptedException {
+        loginPage.adminLogin();
+        InventoryPage inventoryPage = homePage.clickInventory();
+        System.out.println(inventoryPage.getTitle());
+        inventoryPage.setAddProductsBtn();
+        inventoryPage.setPleaseSelectList();
+        inventoryPage.setEmptyProductField();
+        inventoryPage.setEmptyProductFieldWithProduct("346" + Keys.ENTER);
+        inventoryPage.setSubmitProductBtn();
+        inventoryPage.setSearchField("346 1500" + Keys.ENTER);
+        System.out.println("New Product added to the Inventory");
+        inventoryPage.setTrashNewProduct();
+        inventoryPage.alert_getText();
+        String text = inventoryPage.alert_getText();
+        assertEquals(text, "Are you sure you want to delete 1500 forms - 100 pack?", "Wrong Item, don't delete");
+        inventoryPage.alert_clickToAccept();
+        System.out.println("Same New Product removed from the Inventory");
+
+
+
+    }
+
 
 }
