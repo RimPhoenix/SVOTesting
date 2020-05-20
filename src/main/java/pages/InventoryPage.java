89ae5956841209df6e7466b1f0a05911ae296a8a
Robert Moritz <robbiemoritz@yahoo.com>
2020-05-20 18:42:23 -0500

    Inventory tests completed and added to the xml file

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InventoryPage {
@@ -18,21 +19,32 @@ public class InventoryPage {
     }
 
 
-
+       // private By showDataEntries = By.id("DataTables_Table_1_length");
         private By addProductsBtn = By.xpath("//button[@class='btn btn-primary js-confirm-this']");
         private By pleaseSelectList = By.xpath("//span[contains(text(),'Please select')]");
         private By emptyProductField = By.xpath("//html//body//span//span//span//input");
         private By submitProductBtn = By.xpath("//div[@class='modal-footer']//input[@name='commit']");
         private By searchField = By.xpath("//label[contains(text(),'Search:')]//input");
         private By trashNewProduct = By.xpath("//a[contains(@class,'btn text-primary')]//*[local-name()='svg']");
+        private By checkInventoriedItem = By.xpath("//a[contains(text(),'Spray Cleaner 4oz')]");
+        private By checkBatchID = By.xpath("//a[contains(text(),'110')]");
+        private By quantityBefore = By.cssSelector("body.access-grid:nth-child(2) main.container-fluid:nth-child(4) div.batches.show div.card div.card-body table.nowrap.responsive.table.table-sm:nth-child(3) thead:nth-child(1) tr:nth-child(1) > th.text-center:nth-child(3)");
+        private By quantityBeforeAmount = By.cssSelector("body.access-grid:nth-child(2) main.container-fluid:nth-child(4) div.batches.show div.card div.card-body table.nowrap.responsive.table.table-sm:nth-child(3) tbody:nth-child(2) tr:nth-child(1) > td.text-center:nth-child(3)");
+        private By quantityAfter = By.cssSelector("body.access-grid:nth-child(2) main.container-fluid:nth-child(4) div.batches.show div.card div.card-body table.nowrap.responsive.table.table-sm:nth-child(3) thead:nth-child(1) tr:nth-child(1) > th.text-center:nth-child(4)");
+        private By quantityAfterAmount = By.cssSelector("body.access-grid:nth-child(2) main.container-fluid:nth-child(4) div.batches.show div.card div.card-body table.nowrap.responsive.table.table-sm:nth-child(3) tbody:nth-child(2) tr:nth-child(1) > td.text-center:nth-child(4)");
+        private By inventoryItemQuantity = By.id("quantity_inventoried_item-164");
+        private By updateCheckedBtn = By.xpath("//form[@id='new_batch']");
+        private By reasonForChangeList = By.xpath("//select[@id='note_text']");
 
-
-
+//    private Select chooseShownDataEntries() {return new Select(driver.findElement(showDataEntries));}
+//
+//    public void selectNumberOfDataEntries(String option) {WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.presenceOfElementLocated(showDataEntries));
+//        chooseShownDataEntries().selectByVisibleText(option);}
 
     public void setAddProductsBtn(){
         WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.presenceOfElementLocated(addProductsBtn)).click();
-   }
+        wait.until(ExpectedConditions.presenceOfElementLocated(addProductsBtn)).click();   }
 
     public void setPleaseSelectList(){WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.presenceOfElementLocated(pleaseSelectList)).click();}
@@ -44,11 +56,12 @@ public class InventoryPage {
 
     public void setSubmitProductBtn(){driver.findElement(submitProductBtn).click();}
 
-    public void setSearchField(String product){driver.findElement(searchField).sendKeys(product);}
+    public void setSearchField(String product){driver.findElement(searchField).clear();
+        driver.findElement(searchField).sendKeys(product);}
 
     public void setTrashNewProduct(){WebDriverWait wait = new WebDriverWait(driver, 3);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -58,6 +71,35 @@ public class InventoryPage {
     public String alert_getText(){return driver.switchTo().alert().getText();}
 
     public void alert_clickToAccept(){driver.switchTo().alert().accept();}
+
+    public void setCheckInventoriedItem(){WebDriverWait wait = new WebDriverWait(driver, 4);
+        wait.until(ExpectedConditions.elementToBeClickable(checkInventoriedItem)).click(); }
+
+    public void setCheckBatchID(){WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.elementToBeClickable(checkBatchID)).click(); }
+
+    public String setQuantityBefore(){return driver.findElement(quantityBefore).getText();}
+
+    public String setQuantityBeforeAmount(){return driver.findElement(quantityBeforeAmount).getText();}
+
+    public String setQuantityAfter(){return driver.findElement(quantityAfter).getText();}
+
+    public String setQuantityAfterAmount(){return driver.findElement(quantityAfterAmount).getText();}
+
+    public String inventoryItemQuantityValue() {WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.presenceOfElementLocated(inventoryItemQuantity));
+        return driver.findElement(inventoryItemQuantity).getAttribute("value");}
+
+    public void setInventoryItemQuantity(){driver.findElement(inventoryItemQuantity).sendKeys(Keys.ARROW_UP);}
+
+    public void setUpdateCheckedBtn(){WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.elementToBeClickable(updateCheckedBtn)).click();}
+
+    private Select chooseReasonForChange() {return new Select(driver.findElement(reasonForChangeList));}
+
+    public void selectFromReasonForChange(String option) {chooseReasonForChange().selectByVisibleText(option);}
+
+    public void resetInventoryItemQuantity(){driver.findElement(inventoryItemQuantity).sendKeys(Keys.ARROW_DOWN);}
 
 
 }
--- src/test/java/inventory/InventoryTests.java
+++ src/test/java/inventory/InventoryTests.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import pages.InventoryPage;
 import pages.LoginPage;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -30,6 +32,8 @@ public class InventoryTests extends BaseTests {
         loginPage.adminLogin();
         InventoryPage inventoryPage = homePage.clickInventory();
         System.out.println(inventoryPage.getTitle());
+//        String option = "10";
+//        inventoryPage.selectNumberOfDataEntries(option);
         inventoryPage.setAddProductsBtn();
         inventoryPage.setPleaseSelectList();
         inventoryPage.setEmptyProductField();
@@ -39,10 +43,47 @@ public class InventoryTests extends BaseTests {
         System.out.println("New Product added to the Inventory");
         inventoryPage.setTrashNewProduct();
         inventoryPage.alert_getText();
-        String text = inventoryPage.alert_getText();
-        assertEquals(text, "Are you sure you want to delete 1500 forms - 100 pack?", "Wrong Item, don't delete");
+        String alertText = inventoryPage.alert_getText();
+        assertEquals(alertText, "Are you sure you want to delete 1500 forms - 100 pack?", "Wrong Item, don't delete");
         inventoryPage.alert_clickToAccept();
         System.out.println("Same New Product removed from the Inventory");
+        inventoryPage.setSearchField("4oz" + Keys.ENTER);
+        inventoryPage.setCheckInventoriedItem();
+        System.out.println("Now looking at a 4oz bottle page");
+        inventoryPage.setCheckBatchID();
+        System.out.println("Now looking at the batch ID for the 4oz bottle");
+        String quantityBeforeText = inventoryPage.setQuantityBefore();
+        String quantityBeforeAmountText = inventoryPage.setQuantityBeforeAmount();
+        String quantityAfterText = inventoryPage.setQuantityAfter();
+        String quantityAfterAmountText = inventoryPage.setQuantityAfterAmount();
+        System.out.println("Spray Cleaner: " + quantityBeforeText + " " + quantityBeforeAmountText);
+        System.out.println("Spray Clearer: " + quantityAfterText + " " + quantityAfterAmountText);
+        getWindowManager().goBack();
+        getWindowManager().goBack();
+        String inventoryValue = inventoryPage.inventoryItemQuantityValue();
+        assertEquals(inventoryValue, "5", "Not the correct value");
+        inventoryPage.setInventoryItemQuantity();
+        inventoryPage.setUpdateCheckedBtn();
+        System.out.println("Tried to update the Qty but did not give a reason so it didn't work");
+        assertEquals(inventoryValue, "5", "Not the correct value");
+        System.out.println("Quantity is: " + inventoryValue);
+        String reasonForChange = "Stock Refresh";
+        inventoryPage.selectFromReasonForChange(reasonForChange);
+        inventoryPage.setUpdateCheckedBtn();
+        String newInventoryValue = inventoryPage.inventoryItemQuantityValue();
+        assertEquals(newInventoryValue, "6", "Not the correct value");
+        System.out.println("After providing a reason we are allowed to change the Qty");
+        System.out.println("Quantity is: " + newInventoryValue);
+        inventoryPage.resetInventoryItemQuantity();
+        inventoryPage.setUpdateCheckedBtn();
+        String oldInventoryValue = inventoryPage.inventoryItemQuantityValue();
+        assertEquals(oldInventoryValue, "5", "Not the correct value");
+        System.out.println("returned the product Qty to the original number");
+        System.out.println("Quantity is: " + oldInventoryValue);
+
+
+
+
 
 
 
